3135c856f7b5dfc7a10630dce04d0c9a29eb5372
Ujjwal Suman <ujjwalsuman007@gmail.com>
2023-10-13 10:38:56 +0530

    commit 1
    Home page is done and cart page is remaining

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,63 @@
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import CartItem from "../components/CartItem";
+import { useEffect, useState } from "react";
+
+
+
+const Cart = () => {
+
+  const {cart} = useSelector((state) => state);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect (()=>{
+    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price,0));
+  })
+
+  return (
+    <div>
+    {
+      cart.length > 0 ? 
+      (<div>
+
+          <div>
+            {
+              cart.map((item, index) =>{
+                return <CartItem key= {item.id} item= {item} itemIndex={index} />
+              })
+            }
+          </div>
+
+          <div>
+            <div>
+               <div>Your Cart</div>
+               <div>Summary</div>
+               <p>
+                <span>
+                  Total Items : {cart.length}
+                </span>
+               </p>  
+            </div>
+
+            <div>
+              <p>Total Amount: ${totalAmount} </p>
+              <button>
+                CheckOut Now
+              </button>
+            </div>
+          </div>
+      </div>) :
+      (<div>
+        <h1>YourCart is Empty</h1>
+        <NavLink to ="/">
+          <buton>
+            Shop Now
+          </buton>
+        </NavLink>
+      </div>)
+    }
+    </div>
+  );
+};
+
+export default Cart;
